9f2578cb3d000937a32214af78475db9fa14dcb2
kuljot Singh <kuljotsingh30@gmail.com>
2022-02-07 22:57:19 -0500

    Initial Commit

---
+++ Flikx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flikx
+//
+//  Created by Kuljot Singh Chadha on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
