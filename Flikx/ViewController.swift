40d773b0a2fbb4a8c1b4d8ef90ad1f6eb522f2ae
kuljot Singh <kuljotsingh30@gmail.com>
2022-02-12 20:31:40 -0500

    Undo to Last commit

--- Flikx/ViewController.swift
+++ Flikx/ViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 import AlamofireImage
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+            return movies.count
+    }
     
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell=tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        let movie=movies[indexPath.row]
+        let title=movie["title"] as! String
+        let synopsis=movie["overview"] as! String
+        
+        cell.titleLabel.text=title
+        cell.synopsisLabel.text=synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185/"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterUrl!)
+        
+        return cell
+    }
     
 
     @IBOutlet weak var tableView: UITableView!
@@ -22,7 +46,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         tableView.dataSource=self
         tableView.delegate=self
         // Do any additional setup after loading the view.
-        
+        print("Hello")
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -31,10 +55,12 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
              if let error = error {
                     print(error.localizedDescription)
              } else if let data = data {
-                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                 self.movies=dataDictionary["results"] as! [[String:Any]]
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                  
+                 self.movies=dataDictionary["results"] as! [[String:Any]]
                  self.tableView.reloadData()
+                 
+                 print(dataDictionary)
                  
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -44,32 +70,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         }
         task.resume()
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return movies.count
-    }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-            let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
-            
-            print("Hello")
-            
-            let movie=movies[indexPath.row]
-            let title=movie["title"] as! String
-            let synopsis=movie["overview"] as! String
-            
-            cell.titleLabel.text=title
-            cell.synopsisLabel.text=synopsis
-            
-            let baseUrl = "https://image.tmdb.org/t/p/w185/"
-            let posterPath = movie["poster_path"] as! String
-            let posterUrl = URL(string: baseUrl + posterPath)
-            
-            cell.posterView.af.setImage(withURL: posterUrl!)
-            
-            return cell
-    }
 
 }
 
