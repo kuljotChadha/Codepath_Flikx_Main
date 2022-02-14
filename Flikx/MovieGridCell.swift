6a1de1227315e91c73a075c5058fddb482a4aa7b
kuljot Singh <kuljotsingh30@gmail.com>
2022-02-14 15:48:28 -0500

    Flix Part2:Navigation and Passing Data

---
+++ Flikx/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flikx
+//
+//  Created by Kuljot Singh Chadha on 2/13/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var PosterView: UIImageView!
+}
--- Flikx/ViewController.swift
+++ Flikx/ViewController.swift
@@ -34,6 +34,23 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         
         return cell
     }
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        //Find the Selected Movie
+        let cell=sender as! UITableViewCell
+        let indexPath=tableView.indexPath(for:cell)!
+        let movie=movies[indexPath.row]
+        
+        //pass the Selected Movie
+        let detailsViewController=segue.destination as! MovieDetailsViewController; detailsViewController.movie=movie
+        
+        tableView.deselectRow(at: indexPath, animated: true )
+    }
     
 
     @IBOutlet weak var tableView: UITableView!
@@ -46,7 +63,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         tableView.dataSource=self
         tableView.delegate=self
         // Do any additional setup after loading the view.
-        print("Hello")
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
